9117f00085e49f7f6b22df7f804de65429740435
gus-aguiar <gustavopma90@gmail.com>
2022-10-04 15:26:56 -0300

    fiz a segunda

---
+++ script.js
@@ -0,0 +1,18 @@
+// setando cores
+const corUm = 'black'
+const trocaCorUm = document.getElementById('colorUm')
+trocaCorUm.style.backgroundColor = corUm
+
+const corDois = 'green'
+const trocaCorDois = document.getElementById('colorDois')
+trocaCorDois.style.backgroundColor = corDois
+
+const corTres = 'blue'
+const trocaCorTres = document.getElementById('colorTres')
+trocaCorTres.style.backgroundColor = corTres
+
+const corQuatro = 'red'
+const trocaCorQuatro = document.getElementById('colorQuatro')
+trocaCorQuatro.style.backgroundColor = corQuatro
+
+
