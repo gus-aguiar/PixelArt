22e50d22cd9c0dc2e96b2a3dfa90fb4fbff91030
gus-aguiar <gustavopma90@gmail.com>
2022-10-04 16:04:15 -0300

    terceira e quarta

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
 // setando cores
-const corUm = 'black'
+let corUm = 'black'
 const trocaCorUm = document.getElementById('colorUm')
 trocaCorUm.style.backgroundColor = corUm
 
-const corDois = 'green'
+let corDois = 'green'
 const trocaCorDois = document.getElementById('colorDois')
 trocaCorDois.style.backgroundColor = corDois
 
-const corTres = 'blue'
+let corTres = 'blue'
 const trocaCorTres = document.getElementById('colorTres')
 trocaCorTres.style.backgroundColor = corTres
 
-const corQuatro = 'red'
+let corQuatro = 'red'
 const trocaCorQuatro = document.getElementById('colorQuatro')
 trocaCorQuatro.style.backgroundColor = corQuatro
 
+//função que troca de cores
+function geracor(){
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+    return `rgb(${r}, ${g}, ${b})`
+}
+// function atribuicor (){
+//     const pegaPaleta = document.getElementsByClassName('color')
+//     for (let index = 1; index < pegaPaleta.length; index+=1){
+//         pegaPaleta[index].style.backgroundColor = geracor()
+//     }
 
+// }
+
+//evento no botão
+const botao = document.getElementById('button-random-color')
+botao.addEventListener("click", function atribuicor (){
+    const pegaPaleta = document.getElementsByClassName('color')
+    for (let index = 1; index < pegaPaleta.length; index+=1){
+        pegaPaleta[index].style.backgroundColor = geracor()
+    }
+
+})
